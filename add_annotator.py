4c7d1ae7f85fab6d0218e18dab6ecb3e2c6ef183
Erwin Marsi <emarsi@idi.ntnu.no>
2016-02-10 14:04:05 +0100

    v2.0

---
+++ add_annotator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+quick hack to add empty annotations for a new annotator
+
+run from root dir as
+
+./new_annotator.py XX
+"""
+
+import sys
+from os import makedirs
+from glob import glob
+from os.path import basename, splitext
+from os import symlink
+
+annotator = sys.argv[1]
+
+annot_dir = 'data/brat/' + annotator
+
+makedirs(annot_dir)
+
+for txt_fname in glob('data/sent/*.txt'):
+    root =  annot_dir + '/' + splitext(basename(txt_fname))[0] + '#brat_' + annotator
+    symlink('../../sent/' + basename(txt_fname), root + '.txt')
+
+
+
+
+
